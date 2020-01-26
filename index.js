98e04ed6498f045faecf15579c4edf07527ab373
afdemo <60155784+afdemo@users.noreply.github.com>
2020-01-26 18:23:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+// select the elements that have the drum class:
+var buttonCollection = document.querySelectorAll('.drum');
+// convert the returned html-collection into an array:
+var buttonCollectionArray = Array.from(buttonCollection);
+// add an event-listener to each element withthe forEach method:
+buttonCollectionArray.forEach(function(item) {
+  item.addEventListener('click', function() {
+    var buttonInnerHTML = this.innerHTML; //note: 'this' used to capture the specific key press
+    letter = buttonInnerHTML;
+    playAudio(letter);
+    buttonAnimation(letter);
+  });
+});
+
+document.addEventListener('keydown', function(event) {
+  //   alert(event.key + ' was pressed');
+  letter = event.key;
+  playAudio(letter);
+  buttonAnimation(letter);
+});
+
+function playAudio(letter) {
+  switch (letter) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+    case 'k':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+    case 'l':
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    default:
+      console.log(letter);
+  }
+}
+
+function buttonAnimation(letter) {
+  var activeButton = document.querySelector('.' + letter);
+  activeButton.classList.add('pressed');
+  setTimeout(function() {
+    activeButton.classList.remove('pressed');
+  }, 100);
+}
